2b99f646e7475b36a50a6bbc02deaedcf1893ab4
belinecruz <belinematheus388@gmail.com>
2025-03-12 17:48:58 +0000

    ...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,47 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
+  perfil:any = {
     nome: null,
-    poder: null
+    profissao: null,
+    citacao: null,
+    nome_usuario: null,
+    localidade: null,
+    data: null,
+    biografia: null,
+    estatisticas: {
+      curtidas: 0,
+      seguidores: 0,
+      amigos: 0
+    },
+    postagem:[
+      {
+        foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgq.globo.com%2Festilo%2Fbeleza%2Fnoticia%2F2022%2F09%2Fcomo-cuidar-barba-cabelos-brancos.ghtml&psig=AOvVaw3cdV7sNYPJGRSDa20sCnTw&ust=1741885168784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNC-ws2ChYwDFQAAAAAdAAAAABAE',
+        nome: 'carlos aontonio',
+        nome_usuario: '@carlosantonio',
+        texto: 'texto de exemplo',
+        data: '12/3/25'
+  
+      },
+      {
+        foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgq.globo.com%2Festilo%2Fbeleza%2Fnoticia%2F2022%2F09%2Fcomo-cuidar-barba-cabelos-brancos.ghtml&psig=AOvVaw3cdV7sNYPJGRSDa20sCnTw&ust=1741885168784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNC-ws2ChYwDFQAAAAAdAAAAABAE',
+        nome: 'carlos aontonio',
+        nome_usuario: '@carlosantonio',
+        texto: 'jsjxjsnxjsx texto de exemplo',
+        data: '12/3/25'
+  
+      },
+      {
+        foto: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgq.globo.com%2Festilo%2Fbeleza%2Fnoticia%2F2022%2F09%2Fcomo-cuidar-barba-cabelos-brancos.ghtml&psig=AOvVaw3cdV7sNYPJGRSDa20sCnTw&ust=1741885168784000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNC-ws2ChYwDFQAAAAAdAAAAABAE',
+        nome: 'carlos aontonio',
+        nome_usuario: '@carlosantonio',
+        texto: 'texto de exemplo kjnxonxnsxsnxjsnxjsnxs ',
+        data: '12/3/25'
+  
+      }
+    ] 
   };
 
-  constructor( 
-    public crudService: CrudService
-  ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
+  constructor( ){ };
 
 }
